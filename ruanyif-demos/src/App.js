968d5cfcaf8465906a1aecabc85dca8d594b008c
gujunmin <3013366498@qq.com>
2017-03-07 21:58:27 +0800

    update 170307 21:58

--- ruanyif-demos/src/App.js
+++ ruanyif-demos/src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 
 class App extends Component {
     render() {
-        let arr = [
-            <h1>Hello world!</h1>,
-            <h2>React is awesome</h2>
-        ];
-
         return (
-            <div>{arr}</div>
+            <h1>{this.props.title}</h1>
         );
     }
 }
+
+App.propTypes = {
+    title: React.PropTypes.string.isRequired
+};
+
+App.defaultProps = {
+    title: 'Hello World'
+};
 
 export default App;
--- ruanyif-demos/src/index.js
+++ ruanyif-demos/src/index.js
@@ -2,17 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+let data = 123;
+
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
-
-// let arr = [
-//     <h1>Hello world!</h1>,
-//     <h2>React is awesome</h2>
-// ];
-//
-// ReactDOM.render(
-//     <div>{arr}</div>,
-//     document.getElementById('root')
-// );
