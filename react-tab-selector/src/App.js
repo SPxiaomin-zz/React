9afe92a3bae6c6c1ddd7f2cab68bfe958095bb10
gujunmin <3013366498@qq.com>
2017-03-24 17:03:56 +0800

    update 170324 17:03

--- react-tab-selector/src/App.js
+++ react-tab-selector/src/App.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selected: this.props.selected
+        };
+
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+
+    handleOnClick(event) {
+        this.setState({
+            selected: event.target.dataset.value
+        });
+    }
+
     render() {
+        let tabs = this.props.data.map((item) => {
+            let selected = item.value === this.state.selected ?
+                           'selected' : '';
+
+            return (
+                <li
+                    data-value={item.value}
+                    className={selected}
+                    onClick={this.handleOnClick}>
+                    {item.name}
+                </li>
+            );
+        });
+
         return (
-            
+            <div className="tab-selector">
+                <label>{this.props.label}</label>
+                <ul>
+                    {tabs}
+                </ul>
+            </div>
         );
     }
 }
--- react-tab-selector/src/index.js
+++ react-tab-selector/src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+var data = [
+    {name: 'Red', value: 'red'},
+    {name: 'Blue', value: 'blue'},
+    {name: 'Yellow', value: 'yellow'},
+    {name: 'Green', value: 'green'},
+    {name: 'White', value: 'white'}
+];
+
 ReactDOM.render(
-    <App />,
+    <App label="Color" data={data} selected={null} />,
     document.getElementById('root')
 );
