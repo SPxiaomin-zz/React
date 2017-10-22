b368e1377abb0d42b9162701b6d026a8e9e6464a
gujunmin <3013366498@qq.com>
2017-10-22 22:26:17 +0800

    update 1022 22:25

--- webpack/webpack-4/app/Greeter.js
+++ webpack/webpack-4/app/Greeter.js
@@ -1,7 +1,21 @@
-const config = require('./config.json');
+// const config = require('./config.json');
+//
+// module.exports = function() {
+//   let greet = document.createElement('div');
+//   greet.textContent= config.greetText;
+//   return greet;
+// };
 
-module.exports = function() {
-  let greet = document.createElement('div');
-  greet.textContent= config.greetText;
-  return greet;
-};
+import React, { Component } from 'react';
+
+import config from './config.json';
+
+export default class Greeter extends Component {
+  render() {
+    return (
+      <div>
+        {config.greetText}
+      </div>
+    );
+  }
+}
--- webpack/webpack-4/app/main.js
+++ webpack/webpack-4/app/main.js
@@ -1,2 +1,12 @@
-const greeter = require('./Greeter');
-document.querySelector('#root').appendChild(greeter());
+// const greeter = require('./Greeter');
+// document.querySelector('#root').appendChild(greeter());
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Greeter from './Greeter';
+
+ReactDOM.render(
+  <Greeter />,
+  document.querySelector('#root')
+);
